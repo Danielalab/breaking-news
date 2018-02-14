22ab1ab00b2d5d74f987cb1d98d2d43fb851c789
Danielalab <olabolacrayola@hotmail.com>
2018-02-13 23:49:48 -0500

    añade server

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,24 @@ $(document).ready(() => {
   let urlNYtimes = 'http://api.nytimes.com/svc/search/article_search_v2.json?';
   let keyApi = '&api-key=61f1ab32a7874cc7b37a40e9ecc89b49';
 
-  // funcionalidad para crear sección de noticias recientes
-  
+  // funcionalidad para crear sección de noticias recientes usando 'FETCH'
+  let createSectionBreakingNews = () => {
+    let months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    let actualDate = new Date();
+    let day = actualDate.getDate();
+    let actualMonth = months[actualDate.getUTCMonth()];
+    let actualYear = actualDate.getFullYear();
+    let fullDate = actualYear + actualMonth + day;
+    console.log(fullDate);
+    fetch(`${urlNYtimes}begin_date=${fullDate}${keyApi}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   // funcionalidad para buscar noticias
   let addNews = (news) => {
     let articles = news.response;
@@ -37,4 +53,6 @@ $(document).ready(() => {
 
   // asociando eventos a funciones
   form.on('submit', startSearch);
+
+  createSectionBreakingNews();
 });
